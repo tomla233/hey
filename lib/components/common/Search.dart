db99846d207c5fff28c826b9e8229eeef32973e0
tomla233 <tomla233@sina.com>
2026-01-30 11:25:59 +0800

    搜索框组件

---
+++ lib/components/common/Search.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:hey/constant/ColorConstants.dart';
+
+///自定义搜索框
+class Search extends StatelessWidget {
+  final Function(String value) onTextChanged;
+  const Search({super.key, required this.onTextChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      style: const TextStyle(fontSize: 14),
+      decoration: InputDecoration(
+        isDense: true,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide.none,
+        ),
+        // 开启背景填充
+        filled: true,
+        fillColor: const Color(0xFFF3F4F6),
+        hintText: '搜索游戏分区',
+        hintStyle: const TextStyle(
+          color: ColorConstants.primaryGray,
+          fontSize: 14,
+        ),
+        prefixIcon: const Icon(
+          Icons.search,
+          color: ColorConstants.primaryGray,
+          size: 16,
+        ),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+      ),
+      onSubmitted: (value) {
+        FocusScope.of(context).unfocus();
+      },
+      onChanged: (value) {
+        onTextChanged!(value);
+      },
+    );
+  }
+}
--- lib/components/hot/PreferenceSetting.dart
+++ lib/components/hot/PreferenceSetting.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hey/components/common/Search.dart';
 
 class PreferenceSetting extends StatefulWidget {
   const PreferenceSetting({super.key});
@@ -11,8 +12,29 @@ class _PreferenceSettingState extends State<PreferenceSetting> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('偏好设置')),
-      body: const Center(child: Text('偏好设置')),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          '偏好设置',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: SizedBox(
+              height: 30,
+              child: Search(
+                onTextChanged: (String value) {
+                  print('搜索内容变化：$value');
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
